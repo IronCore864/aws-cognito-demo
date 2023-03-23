0ac2de0c14d777272d70f9cdb8514787ea213d4a
Tiexin Guo <guotiexin@gmail.com>
2023-03-23 13:55:49 +0800

    refactor

--- amplify-test/src/App.js
+++ amplify-test/src/App.js
@@ -2,23 +2,10 @@ import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Auth } from '@aws-amplify/auth';
-
-const awsconfig = {
-  region: process.env.REACT_APP_AUTH_REGION,
-  userPoolId: process.env.REACT_APP_AUTH_USER_POOL_ID,
-  userPoolWebClientId: process.env.REACT_APP_AUTH_USER_POOL_WEB_CLIENT_ID,
-  cookieStorage: {
-      domain: process.env.REACT_APP_AUTH_COOKIE_STORAGE_DOMAIN,
-      path: "/",
-      expires: 365,
-      sameSite: "strict",
-      secure: true,
-  },
-  authenticationFlowType: "USER_SRP_AUTH",
-};
+import { awsExports } from './aws-exports';
 
 
-Amplify.configure(awsconfig);
+Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
   Auth.currentSession().then(data => console.log(data.idToken));
